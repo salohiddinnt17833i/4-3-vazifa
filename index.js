1f3d99f7a9cf3322b44f1d79219ccd841f8f5b31
salohiddinnt17833i <salohiddindevoleper@gmail.com>
2024-01-16 22:10:29 +0500

    main

---
+++ index.js
@@ -0,0 +1,160 @@
+
+/**
+ * 1
+ * 3 ta son berilgan ular orasidan 3 ga karralilarining
+ *  kopaytmasini topuvchi algoritm yozing
+ */
+// let num1 = 21;
+// let num2 = 24;
+// let num3 = 70;
+// let sum = 1;
+
+// if (num1 % 3 == 0) {
+//     sum = sum * num1
+// }
+// if (num2 % 3 == 0) {
+//     sum = sum * num2
+// }
+// if (num3 % 3 == 0) {
+//     sum = sum * num3
+// }
+// console.log(sum);
+
+/**
+ * 2 
+ * 3 xonali son berilgan uning raqamlari yigindisini topuvchi algoritm yozing
+ */
+
+
+/**
+ * 3
+ * 3 xonali son berilgan uning toq raqamlari kopaytmasini topuvchi algaritm yzing
+ */
+
+
+
+
+/**
+ * 4
+ *  ta son berilgan uning  orasidan 5 ga hamda 7 ga karralilarining sonini topuvchi algoritm yozing
+ */
+// let num1 = 70;
+// let num2 = 70;
+// let num3 = 70;
+// let counter = 0;
+// if (num1 % 5 == 0) {
+//     if (num1 % 7 == 0) {
+//         counter++
+//     }
+// }
+// if (num2 % 5 == 0) {
+//     if (num2 % 7 == 0) {
+//         counter++
+//     }
+// }
+// if (num3 % 5 == 0) {
+//     if (num3 % 7 == 0) {
+//         counter++
+//     }
+// }
+// console.log(counter);
+
+/**
+ * 5
+ *ixtiyorih harf berilganda uning unli yoki undosh ekanligini aniqlovchi  algoritm yozing
+ */
+
+// let letter = `B`;
+// let result = ` `
+
+// switch (letter) {
+//     case `a`:
+//     case `A`:
+//     case `e`:
+//     case `E`:
+//     case `i`:
+//     case `I`:
+//     case `o`:
+//     case `O`:
+//     case `u`:
+//     case `U`:
+//         result = `${letter} harfi unli harf`
+//         break;
+//     case `b`:
+//     case `B`:
+//     case `d`:
+//     case `D`:
+//     case `f`:
+//     case `F`:
+//     case `g`:
+//     case `G`:
+//     case `h`:
+//     case `H`:
+//     case `j`:
+//     case `J`:
+//     case `k`:
+//     case `K`:
+//     case `l`:
+//     case `L`:
+//     case `m`:
+//     case `M`:
+//     case `n`:
+//     case `N`:
+//     case `p`:
+//     case `P`:
+//     case `q`:
+//     case `Q`:
+//     case `r`:
+//     case `R`:
+//     case `s`:
+//     case `S`:
+//     case `t`:
+//     case `T`:
+//     case `v`:
+//     case `V`:
+//     case `x`:
+//     case `X`:
+//     case `y`:
+//     case `Y`:
+//     case `z`:
+//     case `Z`:
+//         result = `${letter} harfi undosh harf`
+// }
+// console.log(result);
+
+/**
+ * 6
+ * foydalanuvchidan ikita son va 1 ta erifmetik amal va manashuv misolni  bakaring
+ */
+// let num1 = 21;
+// let num2 = 5;
+// let arf = "-";
+// let result;
+
+// switch (arf) {
+//     case "+":
+//         result = num1 + num2;
+//         break;
+//     case "-":
+//         result = num1 - num2;
+//         break;
+//     case "/":
+//         result = num1 / num2;
+//         break;
+//     case "*":
+//         result = num1 * num2;
+//         break;
+// }
+
+// console.log(result);
+
+/**
+ * 7
+ * 3 ta musbat son beriulgan ulardan uchburchak yasash mumkunmi yoki mumkun 
+ * meas shuni biladigan algoritm yasang
+ * */
+
+let num1 = 10;
+let num2 = 10;
+let num3 = 5;
+
